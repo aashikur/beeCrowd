046cb4fc8b00b3c69cc8fc7c97ea2964e76c17c2
Md Ashikur Rahaman <mdaashikur@gmail.com>
2023-04-08 23:34:09 +0600

    Create 1115.c

---
+++ 1115.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+
+    while(1)
+    {
+        scanf("%d %d",&a,&b);
+        if(a> 0 && b> 0)
+            printf("primeiro\n");
+        if(a>0 && b < 0)
+            printf("quarto\n");
+        if(a<0 && b<0)
+            printf("terceiro\n");
+        if(a<0 && b>0)
+            printf("segundo\n");
+        if(a==0 || b == 0)
+            break;
+
+    }
+    return 0;
+}
