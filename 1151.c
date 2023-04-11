ae6806863505e343424f44e5c86efdc557b53ddd
Md Ashikur Rahaman <mdaashikur@gmail.com>
2023-04-11 13:30:51 +0600

    Error Presentaion

---
+++ 1151.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n, a = 0,b = 1, next;
+    scanf("%d",&n);
+
+    for(int i =  1; i<=n ; i++)
+    {
+        printf("%d",a);
+        if(i<n) printf(" ");
+
+        next = a + b;
+        a = b;
+        b = next;
+
+
+    }
+
+
+
+    return 0;
+}
