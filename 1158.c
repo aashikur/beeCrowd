e51ffe73ae5c381b3ab34dfbe64b850de004684a
Md Ashikur Rahaman <mdaashikur@gmail.com>
2023-04-16 20:32:27 +0600

    Create 1158.c

---
+++ 1158.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int t;
+    scanf("%d",&t);
+
+    while(t--)
+    {
+        int X, N = -999999999, count = 0, sum = 0;
+        scanf("%d",&X);
+
+        while(1>N)
+        {
+            scanf("%d",&N);
+        }
+
+        for(int i = X; count < N; i++)
+        {
+            if(i%2 == 1 || i%2 == -1)
+            {
+
+                sum+= i;
+                count++;
+         //     printf("%d ",i ); // For Test
+
+            }
+        }
+
+        printf("%d\n",sum);
+
+    }
+    return 0;
+}
