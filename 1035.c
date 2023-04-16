bbb02fb844510d53e71130d7deeb18d27cbddd63
Md Ashikur Rahaman <mdaashikur@gmail.com>
2023-04-16 19:57:44 +0600

    Create 1035.c

---
+++ 1035.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int A,B,C,D;
+    scanf("%d%d%d%d", &A,&B,&C,&D);
+
+    if(B>C && D>A && C>0 && D>0 && A%2==0 && (C+D) > (A+B))
+    {
+
+        printf("Valores aceitos\n");
+    }
+    else
+    {
+        printf("Valores nao aceitos\n");
+    }
+
+    return 0;
+}
